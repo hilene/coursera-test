d7dd25b9f31f7274c515759e2bc9bd019340404d
Hilene LIN <hlin@iorga.local>
2018-04-19 15:29:13 +0200

    changes

--- app1.js
+++ app1.js
@@ -1,14 +1,22 @@
-    (function () {
+(function() {
     'use strict';
-  var app = angular.module('DIApp', [])  
- app.controller('DIController', function ($scope, $filter){
-     $scope.name = "Hilene";
-     $scope.upper = function (){
-         var upCase = $filter ('uppercase');
-         $scope.name = upCase($scope.name);
-     }
-     
- });
- })();
-
+    var app = angular.module('DIApp', ['ngRoute'])  
+    app.controller('DIController', DIController);
+    function DIController ($scope, $filter, $injector){
+        $scope.name = "Charlotte";
+        $scope.upper = function (){
+            var upCase = $filter ('uppercase');
+            $scope.name = upCase($scope.name);
+        }
+        console.log($injector.annotate(DIController));
                    
+    function AnnotateMe(name, job, blah) {
+        return "blah"
+    }
+    /*  console.log(AnnotateMe());
+    console.log(AnnotateMe);
+    console.log(AnnotateMe.toString());*/
+    console.log(DIController.toString());
+}});
+
+
